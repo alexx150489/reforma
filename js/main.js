7fc0ba6c8c79b9e1be1151e9faabc29c50cf45bd
alexx150489 <nikolskiy150489@gmail.com>
2017-04-26 23:10:59 +0400

    added adaptive menu, footer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
-	
-	
+
+	var openMenu = $('#burger-icon-js');	
+	var menu = $('#navigation-js');
+	var headerTel = $('#header-tel-js');
+	var closeMenu = $('#close-icon-js');
+
+	$(openMenu).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+		headerTel.hide();
+		openMenu.hide();
+		closeMenu.show();
+	});
+
+	$(closeMenu).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+		headerTel.show();
+		closeMenu.hide();
+		openMenu.show();
+	});
+
+
 	
 });

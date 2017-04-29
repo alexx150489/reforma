265b1acf3ef85df6bc71d6155235841acd5c34b7
alexx150489 <nikolskiy150489@gmail.com>
2017-04-30 01:05:44 +0400

    fix main page

--- js/main.js
+++ js/main.js
@@ -24,6 +24,28 @@ $(document).ready(function() {
 		openMenu.show();
 	});
 
+// slider http://github.com/kenwheeler/slick slick.js >>>
 
+// main-slider
+	$('.main-slider-js').slick({
+		autoplay: true,
+		pauseOnHover: true,
+		autoplaySpeed: 3000,
+		dots: true,
+		arrows: false,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
 	
+// actions-slider
+	$('.actions-slider-js').slick({
+		autoplay: true,
+		pauseOnHover: true,
+		autoplaySpeed: 3000,
+		dots: true,
+		arrows: true,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+
 });

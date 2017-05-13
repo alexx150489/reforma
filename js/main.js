8ce6c09218b1d82bcef28069d04c04b75bb77501
alexx150489 <nikolskiy150489@gmail.com>
2017-05-14 01:52:08 +0400

    added js features

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 
-// mobile-menu >>>
+// mobile-menu open/close >>>
 
 	var openMenu = $('#burger-icon-js');	
 	var menu = $('#navigation-js');
@@ -22,6 +22,22 @@ $(document).ready(function() {
 		headerTel.show();
 		closeMenu.hide();
 		openMenu.show();
+	});
+
+// main menu contacts toggle >>>
+
+	$('.menu-town-js').on("click", function(e) {
+		e.preventDefault();
+		$('.menu-town-js').removeClass("active");
+		$(this).addClass("active");
+		$('.menu-tel-js').hide();
+
+		if ( $(this).text() === 'Москва' ) {
+			$('.menu-tel-js.msk').show();
+		} else if ( $(this).text() === 'Екатеринбург' ) {
+			$('.menu-tel-js.ekb').show();
+		}
+
 	});
 
 // slider http://github.com/kenwheeler/slick slick.js >>>
@@ -69,7 +85,38 @@ $(document).ready(function() {
 		'overlayShow'	:	false,
 		'margin': 0,
 		'padding' : 0
+	});
 
+	// $(".lic1-js").fancybox({
+	// 	'transitionIn'	:	'elastic',
+	// 	'transitionOut'	:	'elastic',
+	// 	'speedIn'		:	600, 
+	// 	'speedOut'		:	200, 
+	// 	'overlayShow'	:	false,
+	// 	'margin': 0,
+	// 	'padding' : 0
+	// });
+
+	// model-block menu
+	// $('body').on("click", function(e) {
+	// 	if ( $('.model-point-js').hasClass("active") ) {
+	// 		$('.model-point-js').removeClass("active");
+	// 	}
+	// });
+
+	$(document).click( function(e) {
+
+		if( $(e.target).closest(".model-point").length ) {
+			return;
+		}
+
+		$(".model-point").removeClass("active");
+		event.stopPropagation();
+	});
+
+	$('.model-point').click( function(e) {
+		$(".model-point").removeClass("active");
+		$(this).toggleClass("active");
 	});
 
 });
